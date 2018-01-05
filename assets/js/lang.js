29c2b7c92d3b72238956fc7b3fd62670d41ea32f
Eugene <daimontouch@ya.ru>
2018-01-05 19:51:11 +0500

    Итерация 11
    Адаптивность футера по макетам, вставка графики в новой цветовой гамме, добавление предварительных текстов в блоки, вынос слайдера "преимуществ" в отдельный блок

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -14,9 +14,9 @@ var dic = {
         'ask_question' : 'ASK QUESTION',
         'main_about' : 'Operating system for synchronization / integration of real economy<br/>with digital intransport / infrastructural projects and on the global service market',
         'more' : 'more',
-        'concept' : 'concept',
-        'blockchain_signup' : 'sign up in blockchain',
-        'subscribe' : 'subscribe',
+        'concept' : 'CONCEPT',
+        'blockchain_signup' : 'SIGH UP<br/>IN BLOCKCHAIN',
+        'subscribe' : 'SUBSCRIBE',
         'choose_language' : 'Choose your language',
         'current_block' : 'Current block number'
     },
@@ -33,9 +33,9 @@ var dic = {
         'ask_question' : 'ЗАДАТЬ ВОПРОС',
         'main_about' : 'операционная система для синхронизации/объединения реальной экономики<br/>c цифровой в транспортных/инфрасруктурных проектах и на глобальном рынке услуг',
         'more' : 'подробнее',
-        'concept' : 'концепция',
-        'blockchain_signup' : 'зарегистрироваться в блокчейн',
-        'subscribe' : 'подписаться',
+        'concept' : 'КОНЦЕПЦИЯ',
+        'blockchain_signup' : 'ЗАРЕГИСТРИРОВАТЬСЯ<br/>В БЛОКЧЕЙН',
+        'subscribe' : 'ПОДПИСАТЬСЯ',
         'choose_language' : 'Выберите свой язык',
         'current_block' : 'Номер текущего блока'
     }
